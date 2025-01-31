9cd797387e2f8945019cf42ed4cf131902fd40eb
Siddharth <bhattsiddharth3011@gmail.com>
2025-01-31 10:47:54 +0530

    Created context component instead of prop drilling and calling api from App jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,12 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./Components/Header/Header";
 import Dishes from "./Components/Dishes/Dishes";
 import DisheDetails from "./Components/DishDetails";
-import axios from "axios";
 import DishSuggester from './Components/DishSuggester'
 import { DishProvider } from "./Components/DishProvider";
 
 function App() {
-  const [dishes, setDishes] = useState([])
-
-  const fetchDishes = () => {
-    axios.get('http://localhost:8000/api/dishes')
-      .then((response) => {
-        setDishes(response.data)
-      })
-      .catch((error) => console.log('Error while fetching =>', error))
-  }
-
-  useEffect(() => {
-    fetchDishes()
-  }, [])
-
   return (
     <>
       <DishProvider>
